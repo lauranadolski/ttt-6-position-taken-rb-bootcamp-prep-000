d2792032e18f36bd95df5134e9bee23ac4005ee8
Laura Nadolski <laura.nadolski@gmail.com>
2018-03-28 04:20:53 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,41 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  position = board[index]
+  position ||
+  end
+end
+
+
+
+# if board[index] == ' ' || '' || nil
+#     return false
+#   else
+#     return true
+#   end
+
+  # if board[index] == "X"
+  #   true  
+  # elsif board[index] == "O"
+  #   true
+  # elsif board[index] == " "
+  #   false
+  # elsif board[index] != ""
+  #   false
+  # else
+  #   false
+  # end
+  
+  
+  
+  # if position == "X"
+  #   true
+  # elsif position == "O"
+  #   true
+  # elsif position == " "
+  #   false
+  # elsif position == ""
+  #   false
+  # else
+  #   false
+  # end
