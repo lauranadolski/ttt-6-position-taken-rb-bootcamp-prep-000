163e446a2a21e01a8e445f547caad84a2068d3c8
Laura Nadolski <laura.nadolski@gmail.com>
2018-03-28 04:24:09 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 
 def position_taken?(board, index)
   position = board[index]
-  !(position == " " || position == "" || position == nil
+  !(position == " " || position == "" || position == nil)
 end
 
 
