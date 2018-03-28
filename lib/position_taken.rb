7c8a65928deb50a28ed9379790f76855b39e7c4c
Laura Nadolski <laura.nadolski@gmail.com>
2018-03-28 04:24:51 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,37 +4,3 @@ def position_taken?(board, index)
   position = board[index]
   !(position == " " || position == "" || position == nil)
 end
-
-
-
-# if board[index] == ' ' || '' || nil
-#     return false
-#   else
-#     return true
-#   end
-
-  # if board[index] == "X"
-  #   true  
-  # elsif board[index] == "O"
-  #   true
-  # elsif board[index] == " "
-  #   false
-  # elsif board[index] != ""
-  #   false
-  # else
-  #   false
-  # end
-  
-  
-  
-  # if position == "X"
-  #   true
-  # elsif position == "O"
-  #   true
-  # elsif position == " "
-  #   false
-  # elsif position == ""
-  #   false
-  # else
-  #   false
-  # end
